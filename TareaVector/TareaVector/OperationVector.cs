b4010dc633f6504b80580f9576542b3c8ccea178
Sergio Lopez <Sergio.Lopez@fundacion-jala.org>
2019-02-26 16:34:10 -0400

    Tarea resuelta

---
+++ TareaVector/TareaVector/OperationVector.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TareaVector
+{
+    public sealed class OperationVector
+    {
+        private static OperationVector instance = null;
+        private int[] sumTotalVector;
+        public int SizeVector;
+        private OperationVector()
+        {
+            instance = null;
+            SizeVector = 0;
+        }
+        public static OperationVector Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    if (instance == null)
+                    {
+                        instance = new OperationVector();
+                    }                   
+                }
+                return instance;
+            }
+        }
+        public int[] TransformArray(string data)
+        {
+            string[] res = data.Split(',');
+            int[] vecInit = new int[SizeVector];
+            int cont = 0;
+            foreach (string item in res)
+            {
+                vecInit[cont] = Int32.Parse(item);
+                cont++;
+            }
+            return vecInit;
+        }
+        public int[] SumArray(int[]dataVector)
+        {
+            int[] result = new int[SizeVector];
+            for (int i = 0; i < SizeVector; i++)
+            {
+                sumTotalVector[i] += dataVector[i];
+            }
+            return sumTotalVector;
+        }
+        public void printVector()
+        {
+            for (int i = 0; i < SizeVector; i++)
+            {
+                Console.WriteLine(sumTotalVector[i]);
+            }
+        }
+        public void VectorMoreLarge(string[] args)
+        {
+            foreach (var item in args)
+            {
+                string[] res = item.Split(',');
+                if(SizeVector < res.Length)
+                {
+                    SizeVector = res.Length;
+                }
+            }
+            sumTotalVector = new int[SizeVector];
+        }
+
+    }
+}
---
+++ TareaVector/TareaVector/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TareaVector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] datas = args;
+            var operation = OperationVector.Instance;
+            operation.VectorMoreLarge(datas);
+            foreach (var data in datas)
+            {
+                int[] vec = operation.TransformArray(data);
+                int[] result = operation.SumArray(vec);
+            }
+            operation.printVector();
+        }
+    }
+}
